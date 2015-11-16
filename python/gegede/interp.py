5876bf5bd0361ecc17cf3a1fe1eb5a0046ee3dd1
Brett Viren <bv@bnl.gov>
2015-11-16 10:09:54 -0500

    v0.3, fix problem of two builders sharing the same subbuilder

--- python/gegede/interp.py
+++ python/gegede/interp.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
-'''Some functions that to the interpretation of evaluated
+'''Some functions that do the interpretation of evaluated
 configuration data into builder objects.
 '''
-
-def get_builder_config(dat, name):
-    '''
-    Return the builder's configuration section of the given <name>.
-
-    If <name> is None, return first builder in <dat>.
-    '''
-    if name is None:
-        return dat.items()[0]
-    return name, dat[name]
 
 def make_builder(dat, name = None):
     '''Return the a builder object of the given name from the
     configuration data <dat> and recursively call on any subbuilders.
     If no name is given return the first found.
+
+    This consumes the 'class' and 'subuilders' entries of the configuration dat object.
     '''
-    bname, bdat = get_builder_config(dat, name)
-    klass = bdat.pop('class')
+    builder_objects = dict()
+    if name is None:
+        name = dat.items()[0][0]
+
+    top = walk_builder_config_graph(dat, name, builder_objects)
+    return top
+    
+def walk_builder_config_graph(dat, bname, builder_objects):
+    '''Recur through the DAG of builder configs'''
+
+    if bname in builder_objects:
+        # been here.
+        return builder_objects[bname]
+
+    try:
+        bdat = dat[bname]
+    except KeyError,e:
+        print 'No such builder configuration section: "bname"' % bname
+        raise
+    klass = bdat.pop('class')   # make exactly one instance
     bobj = klass(bname)
+    builder_objects[bname] = bobj;
+
     subbuilders = bdat.pop('subbuilders',list())
     for sbname in subbuilders:
-        sb = make_builder(dat, sbname)
+        sb = walk_builder_config_graph(dat, sbname, builder_objects)
         bobj.builders[sbname] = sb
     return bobj
     
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name = 'gegede',
-      version = '0.2',
+      version = '0.3',
       description = 'General Geometry Description',
       author = 'Brett Viren',
       author_email = 'brett.viren@gmail.com',
